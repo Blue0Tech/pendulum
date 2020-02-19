fce42279193edc0674fa0208e74a3612f044ba49
Pruthvi Shrikaanth <shrikaanthpruthvispareemail@gmail.com>
2020-02-19 19:25:15 +0000

    Made bar

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+var options;
+
+var bar;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  options = {
+    isStatic : true
+  }
+  bar = Bodies.rectangle(200,100,250,10,options);
+  World.add(world, bar);
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background("black");
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(bar.position.x,bar.position.y,bar.bounds.max.x-bar.bounds.min.x,bar.bounds.max.y-bar.bounds.min.y);
 }
